167af0c966e9399683f1e5f320ff4eb754baddfe
Hectora63 <hectora63@nycstudents.net>
2025-11-17 14:42:42 -0500

    Add files via upload
    11/17 update

--- src/Actions.java
+++ src/Actions.java
@@ -3,5 +3,17 @@ public class Actions {
     private int yPos=0;
 
 
-    
+public Actions (String input){
+if(input.equals("Forward")){
+    Forward();
+}
+System.out.println(xPos);
+    System.out.println(yPos);
+}
+
+void  Forward()
+{
+    yPos += 1;
+
+}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        World current = new World();
-        System.out.println();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("in");
+  String inputAction=scan.next();
+     new Actions(inputAction);
+        scan.close();
+
     }
 }
