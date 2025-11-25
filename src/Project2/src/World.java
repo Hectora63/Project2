3e36d854955bd032d6fbc8d8bdddd1d8acca6b81
Hectora63 <hectora63@nycstudents.net>
2025-11-25 10:48:19 -0500

    Add files via upload
    11/25 lil sum before class

---
+++ src/Project2/src/World.java
@@ -0,0 +1,25 @@
+
+
+public class World {
+
+    public World()
+    {
+
+    }
+
+
+void test(int x,int y)
+{
+    if(Math.abs(x)==3)
+    {
+        System.out.println("river here");
+        x=2;
+    }
+    if(Math.abs(y)==1)
+    {
+        System.out.println("river here");
+        y=0;
+    }
+}
+
+    }
